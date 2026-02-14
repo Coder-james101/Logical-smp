95710c8390b252bb6955ab260ffba8ef4a2a65b2
Coder-james101 <jamesandcolyer4@gmail.com>
2026-02-15 00:09:21 +1100

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,50 @@ zoomedContainer.addEventListener("click", (e) => {
         zoomedImg.style.display = "none";
     }
 });
+
+
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+
+const lineX1 = document.getElementById("lineX1");
+const lineX2 = document.getElementById("lineX2");
+
+
+function turnHamburgerToX() {
+    line1.style.animation = "line1X 0.4s forwards";
+    line2.style.animation = "line2X 0.4s forwards";
+    line3.style.animation = "line3X 0.4s forwards";
+}
+
+
+function turnHamburgerToHam() {
+    line1.style.animation = "line1X 0.4s backwards";
+    line2.style.animation = "line2X 0.4s backwards";
+    line3.style.animation = "line3X 0.4s backwards";
+}
+
+
+
+const menu = document.getElementById("menu");
+
+let pressed = false;
+// Example: trigger on click
+document.getElementById("menu_open_container").addEventListener("click", ()=>{
+    if(!pressed){
+        turnHamburgerToX()
+        menu.style.right = "0"
+        pressed = true;
+    }
+
+    else{
+        turnHamburgerToHam()
+        pressed = false;
+        menu.style.right = "100vw"
+    }
+ 
+    
+
+});
+
+
